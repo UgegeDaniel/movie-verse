c0417b33cf12d6459c1da37f9e0e01e0b4d3c98c
Ugege Daniel <ugege62@gmail.com>
2022-09-01 08:46:20 +0100

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Categories } from './categories/Categories';
+export { default as Header } from './header/Header';
+export { default as HeroControls } from './heroControls/HeroControls';
+export { default as HeroMovie } from './heroMovie/HeroMovie';
+export { default as MovieInfo } from './movieInfo/MovieInfo';
+export { default as MovieList } from './movieList/MovieList';
