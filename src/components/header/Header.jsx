d0a65ae9fef87a5c0d87b447386d36b186a251da
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-09-01 10:30:25 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import './HeaderStyle.css'
 import {FaSearch} from 'react-icons/fa'
-import Avatar from '../../assests/img/avatar.png';
 const Header = ({searchTerm, setSearchTerm}) => {
 return(
 <header classame='header'>
-<div className='title'>
+	<div className='title'>
 	<h1>Movie</h1><span>Verse</span>
-</div>
+	</div>
 	<div className='header-right'>
-<div className='search-bar'>
-		<FaSearch className='search'/>
-		<input className='search-input' placeholder="search your favorite movie" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
+	<div className='search-bar'>
+	<FaSearch className='search'/>
+	<input className='search-input' placeholder="search your favorite movie" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
 </div>
-		<img src={Avatar} alt='avater'/>
 	</div>
 </header>
 )
