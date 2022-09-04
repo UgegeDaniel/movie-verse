b7d5b9c9e8c6f3f4419978f38d1b13471385cf81
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-09-04 01:18:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ const [type, setType] = useState({tv:"movie", movieId:""})
 const [movieInfo, setMovieInfo] = useState({show:false, movie:{}})
 const [allMovies, setAllMovies] = useState([])
 const [filteredMovies, setFilteredMovies]= useState([])
+const [screenSize, setScreenSize] = useState(0);
+const [isMobile, setIsMobile] = useState(true);
 const {loading} = useFetch();
-  const { data: trending } = useFetch('/trending/all/day')
-  const { data: topRated } = useFetch('/movie/top_rated')
-  const { data: airingToday } = useFetch('/tv/airing_today')
-  const { data: upComing } = useFetch('/movie/upcoming')
-  const { data: nowPlaying } = useFetch('/movie/now_playing')
-  const { data: searchResults } = useSearch('/search/movie', searchTerm)
-  const { data: genres} = useFetch('/genre/movie/list')
-  const { data: trailers } = useTrailers(`/${type.tv}/${type.movieId}`)
+const { data: trending } = useFetch('/trending/all/day')
+const { data: topRated } = useFetch('/movie/top_rated')
+const { data: airingToday } = useFetch('/tv/airing_today')
+const { data: upComing } = useFetch('/movie/upcoming')
+const { data: nowPlaying } = useFetch('/movie/now_playing')
+const { data: searchResults } = useSearch('/search/movie', searchTerm)
+const { data: genres} = useFetch('/genre/movie/list')
+const { data: trailers } = useTrailers(`/${type.tv}/${type.movieId}`)
 
 const getTrailer = (videos) => {
   const officialTrailer = videos?.find((video)=>video.name === "Official Trailer")
@@ -28,32 +30,30 @@ const getTrailer = (videos) => {
 useEffect(()=>{
 setAllMovies(allMovies.concat(trending, topRated, airingToday))
 }, [trending, topRated, airingToday])
-
+  
 const randomHeroMovie = randomMovie(allMovies)
 const trailerUrl = getTrailer(trailers)
 
   return (
-<div className='App'>
-<Header searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-<Categories handleGenre={handleGenre} genres={genres}/>
-<HeroMovie movie={randomHeroMovie}/>
-<HeroControls/>
-{searchTerm ?
-<MovieList title='Search Results' movies={searchResults} setMovieInfo={setMovieInfo}/>
-: (filteredMovies.length > 0) &&
-<MovieList title='Search Results' movies={filteredMovies} setMovieInfo={setMovieInfo}/>
-}
-<MovieList title='Trending Movies' movies={trending} setMovieInfo={setMovieInfo}/>
-<MovieList title='Top Rated Movies' movies={topRated} setMovieInfo={setMovieInfo}/>
-<MovieList title='Airing Today' movies={airingToday}setMovieInfo={setMovieInfo}/>
-<MovieList title='Upcoming Movies' movies={upComing} setMovieInfo={setMovieInfo}/>
-<MovieList title='In Cinemas Now' movies={nowPlaying} setMovieInfo={setMovieInfo}/>
-{
-movieInfo.show && <MovieInfo movie={movieInfo.movie} setMovieInfo={setMovieInfo} setType={setType} trailerUrl={trailerUrl}/>
-}
-
-</div>
+    <div className='App'>
+     <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+     <Categories handleGenre={handleGenre} genres={genres}/>
+     <HeroMovie movie={randomHeroMovie}/>
+     <HeroControls/>
+     {searchTerm ?
+       <MovieList title='Search Results' movies={searchResults} setMovieInfo={setMovieInfo}/>
+     : (filteredMovies.length > 0) &&
+       <MovieList title='Search Results' movies={filteredMovies} setMovieInfo={setMovieInfo}/>
+     }
+     <MovieList title='Trending Movies' movies={trending} setMovieInfo={setMovieInfo}/>
+     <MovieList title='Top Rated Movies' movies={topRated} setMovieInfo={setMovieInfo}/>
+     <MovieList title='Airing Today' movies={airingToday}setMovieInfo={setMovieInfo}/>
+     <MovieList title='Upcoming Movies' movies={upComing} setMovieInfo={setMovieInfo}/>
+     <MovieList title='In Cinemas Now' movies={nowPlaying} setMovieInfo={setMovieInfo}/>
+     {movieInfo.show && 
+       <MovieInfo movie={movieInfo.movie} setMovieInfo={setMovieInfo} setType={setType} trailerUrl={trailerUrl}/>
+      }
+    </div>
   )
 }
-
 export default App
